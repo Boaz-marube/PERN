c43ecade181123775a6de866918163f7f4cc89f2
Boaz Marube <boazmarube1@gmail.com>
2025-10-29 14:30:53 +0300

    Added postgredb

---
+++ backend/config/config.js
@@ -0,0 +1,7 @@
+import {neon} from "@neondatabase/serverless"
+import dotenv from "dotenv"
+
+dotenv.config();
+const {PGHOST, PGDATABASE, PGUSER, PGPASSWORD} = process.env
+
+export const sql = neon(`postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require&channel_binding=require`)
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ import morgan from "morgan"
 import cors from "cors"
 import dotenv from "dotenv"
 
+import productRoutes from "./routes/productRoutes.js"
+import {sql} from './config/config.js'
+
 dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5002
@@ -13,6 +16,29 @@ app.use(cors())
 app.use(helmet()) 
 app.use(morgan("dev")) // logging
 
-app.listen(5002, () => {
-    console.log(`Server is running on port ${PORT}`)
+app.use("/api/products", productRoutes)
+
+async function initDB(){
+    try {
+        await sql`
+            CREATE TABLE IF NOT EXISTS products(
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            image VARCHAR(255) NOT NULL,
+            price DECIMAL(10, 2) NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+       
+        )`
+        console.log('DB Initialised successfully')
+    } catch (error) {
+        console.error(error)
+    }
+    
+}
+initDB().then(() => {
+    app.listen(5002, () => {
+        console.log(`Server is running on port ${PORT}`)
+})
+
 })
