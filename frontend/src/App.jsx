5ba6447d21d7e5fcad7d5b51f9e0c37fcd92a86d
Boaz Marube <boazmarube1@gmail.com>
2025-10-30 10:22:11 +0300

    Added NavBar and Theme Selector

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,28 @@
-import Navbar from './components/Navbar'
-import './index.css'
+import Navbar from "./components/Navbar";
+
+import HomePage from "./pages/Homepage.jsx";
+import ProductPage from "./pages/Productpage.jsx";
+
+import { Routes, Route } from "react-router-dom";
+import { useThemeStore } from "./store/useThemeStore";
+
+import { Toaster } from "react-hot-toast";
 
 function App() {
-  return(
-    <div className="min-h-screen bg-base-200 transition-colors duration-300">
-      <Navbar/>
+  const { theme } = useThemeStore();
+
+  return (
+    <div className="min-h-screen bg-base-200 transition-colors duration-300" data-theme={theme}>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product/:id" element={<ProductPage />} />
+      </Routes>
+
+      <Toaster />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
