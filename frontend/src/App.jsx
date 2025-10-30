988a28a9e41afd60173f81c9d69ee4ae6b355dfd
Boaz Marube <boazmarube1@gmail.com>
2025-10-30 09:53:17 +0300

    Initialized frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from './components/Navbar'
+import './index.css'
+
+function App() {
+  return(
+    <div className="min-h-screen bg-base-200 transition-colors duration-300">
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
